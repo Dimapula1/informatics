8d40b2b314f6ec26ba70e26ccbb23ada7a9911b2
Dimapula1 <dmitryglumov48@gmail.com>
2024-10-16 10:51:21 +0500

    Update task2.py

--- lab1/task2.py
+++ lab1/task2.py
@@ -1,1 +1,7 @@
-
+from math import cos, log, exp, sin
+x=float(input("Введите значение пременной x: "))
+y=float(input("Введите значение пременной y: "))
+z=float(input("Введите значение пременной z: "))
+a=(x**3/2)**(1/2)-sin(y)
+b=exp(2)/3-cos(y)+z+log(y)
+print(f"Получено значение функции a={a}")
