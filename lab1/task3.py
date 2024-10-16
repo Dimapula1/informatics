b1662b358070a36b707cc76212fea0d26b5dcf6f
Dimapula1 <dmitryglumov48@gmail.com>
2024-10-16 11:02:23 +0500

    Update task3.py

--- lab1/task3.py
+++ lab1/task3.py
@@ -1,1 +1,6 @@
-
+from math import cos, log, exp, sin
+x=float(input("Введите значение пременной x: "))
+a=float(input("Введите значение пременной a: "))
+b=float(input("Введите значение пременной b: "))
+z=(((x)**3)**(1/4))/(x+a)+x**b
+print(f"Получено значение функции z={z}")
